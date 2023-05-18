2d30bdbcc183d6a309917a1eef0dd7e15af04a39
michaelamponsah <amponsahmichaelkwame@gmail.com>
2023-05-18 16:28:50 +0000

    Setup basic app layout

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Layout = ({ children }) => (
-  <section>
-    <aside>
+  <section className="row">
+    <aside className="col-md-2 bg-primary vh-100">
       Side Navigation
     </aside>
-    <div>
+    <div className="col-md-10">
       {children}
     </div>
   </section>
