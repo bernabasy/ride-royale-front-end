032f5ef76fc0ad547398bc8eb57fe79b2a4e236b
michaelamponsah <amponsahmichaelkwame@gmail.com>
2023-05-19 07:44:06 +0000

    Refactor code to fit UI specifications

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,27 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import { FaBars, FaRegClosedCaptioning } from 'react-icons/fa';
 
 import style from './style.module.css';
 
-const Layout = ({ children }) => (
-  <section className="row gx-0">
-    <nav className={`container-fluid navbar navbar-light bg-light shadow-sm ${style['status-bar']}`}>
-      <button
-        type="button"
-        className="btn"
-      >
-        Menu
-      </button>
-    </nav>
+const Layout = ({ children }) => {
+  const [sidebarVisible, setSidebarVisible] = useState(false);
 
-    <aside className={`col-md-2 bg-primary vh-100 ${style['aside-wrapper']}`}>
-      Side Navigation
-    </aside>
-    <div className="col-md-10">
-      {children}
-    </div>
-  </section>
-);
+  const handleMenuClick = () => {
+    setSidebarVisible((sidebarVisible) => !sidebarVisible);
+  };
+
+  return (
+    <section className="row gx-0">
+      <nav className={`container-fluid navbar navbar-light bg-light shadow-sm ${style['status-bar']}`}>
+        <div className="row d-flex align-items-center justify-content-start">
+          <div className="col">
+            <button
+              type="button"
+              className="btn"
+              onClick={handleMenuClick}
+            >
+              <FaBars style={{ fontSize: '1.5rem' }} />
+            </button>
+          </div>
+        </div>
+      </nav>
+      <aside className={`col-md-2 bg-light container-fluid vh-100 ${sidebarVisible ? style['aside-visible'] : style['aside-hidden']}`}>
+        <div className="row d-flex align-items-center justify-content-start">
+          <div className="col">
+            <button
+              type="button"
+              className="btn"
+              onClick={handleMenuClick}
+            >
+              <FaRegClosedCaptioning style={{ fontSize: '1.5rem' }} />
+            </button>
+          </div>
+        </div>
+      </aside>
+      <div className="col-md-10">
+        {children}
+      </div>
+    </section>
+  );
+};
 
 Layout.propTypes = {
   children: PropTypes.element.isRequired,
