d0f1ca4b8a7a55750bcaf6a9c87de4dccf74cce4
michaelamponsah <amponsahmichaelkwame@gmail.com>
2023-05-19 03:49:27 +0000

    Implement mobile status bar

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import style from './style.module.css';
+
 const Layout = ({ children }) => (
   <section className="row gx-0">
-    <aside className="col-md-2 bg-primary vh-100">
+    <nav className={`container-fluid navbar navbar-light bg-light shadow-sm ${style['status-bar']}`}>
+      <button
+        type="button"
+        className="btn"
+      >
+        Menu
+      </button>
+    </nav>
+
+    <aside className={`col-md-2 bg-primary vh-100 ${style['aside-wrapper']}`}>
       Side Navigation
     </aside>
     <div className="col-md-10">
