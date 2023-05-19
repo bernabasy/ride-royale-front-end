09439bd4171a493081c86a58be2552debe85a63f
michaelamponsah <amponsahmichaelkwame@gmail.com>
2023-05-19 14:22:35 +0000

    Implement side nav links

--- src/components/SideNav/index.jsx
+++ src/components/SideNav/index.jsx
@@ -1,10 +1,30 @@
 import React from 'react';
+import {
+  MdHome,
+  MdDirectionsCar,
+  MdOutlineEventNote,
+  MdOutlineAdd,
+  MdOutlineDelete,
+} from 'react-icons/md';
 import SideNavItem from '../SideNavItem';
 
 const SideNav = () => (
   <ul className="list mt-4">
-    <SideNavItem title="Home" />
-    <SideNavItem title="New Reservation" />
+    <SideNavItem title="Home">
+      <MdHome className="icon" />
+    </SideNavItem>
+    <SideNavItem title="New Reservation">
+      <MdDirectionsCar className="icon" />
+    </SideNavItem>
+    <SideNavItem title="My Reservations">
+      <MdOutlineEventNote className="icon" />
+    </SideNavItem>
+    <SideNavItem title="Add Car">
+      <MdOutlineAdd className="icon" />
+    </SideNavItem>
+    <SideNavItem title="Delete Car">
+      <MdOutlineDelete className="icon" />
+    </SideNavItem>
   </ul>
 );
 
--- src/components/SideNavItem/index.jsx
+++ src/components/SideNavItem/index.jsx
@@ -2,14 +2,16 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import style from './style.module.css';
 
-const SideNavItem = ({ title }) => (
+const SideNavItem = ({ title, children }) => (
   <li className={`list-group-item p-2 ${style['side-nav-item']}`}>
-    {title}
+    {children}
+    <span className={` ${style['side-nav-span']}`}>{title}</span>
   </li>
 );
 
 SideNavItem.propTypes = {
   title: PropTypes.string.isRequired,
+  children: PropTypes.element.isRequired,
 };
 
 export default SideNavItem;
