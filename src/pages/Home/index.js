0115d36818b4465219518cd88c9eb83f12145f36
bernabasy <104406349+bernabasy@users.noreply.github.com>
2023-05-22 16:21:24 +0300

    Fix linters error

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Layout from '../../components/Layout/Layout';
-import CarList from '../../components/CarList/CarList';
+import CarList from '../../components/CarsList/CarsList';
 
 const Home = () => (
   <Layout>
