b1ea5b8a2ed3624f6f144df8f205f4e1145d13d9
bernabasy <104406349+bernabasy@users.noreply.github.com>
2023-05-22 15:43:05 +0300

    Add configureStore

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,1 +1,11 @@
 // Redux store configurations go here
+import { configureStore } from '@reduxjs/toolkit';
+import usersReducer from './userSlice'
+
+const store = configureStore({
+  reducer: {
+    user: usersReducer,
+  },
+});
+
+export default store;
