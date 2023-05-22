4e5823d923301085cbab2768417eb48320379265
bernabasy <104406349+bernabasy@users.noreply.github.com>
2023-05-22 17:32:30 +0300

    fix  linters

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Layout from '../../components/Layout/Layout';
+// eslint-disable-next-line import/extensions
 import CarsList from '../../components/CarsList/CarsList';
 
 const Home = () => (
