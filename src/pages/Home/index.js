2b320e18938a52d45d79c13678fb4e81b425d706
bernabasy <104406349+bernabasy@users.noreply.github.com>
2023-05-22 15:17:04 +0300

    Link Car list Jsx to Layout

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import Layout from '../../components/Layout/Layout';
+import CarList from '../../components/CarsList/CarsList';
 
 const Home = () => (
   <Layout>
-    <div>Home</div>
+    <CarList />
   </Layout>
 );
 
