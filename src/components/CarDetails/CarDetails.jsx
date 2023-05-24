f29c05f1b21114bfa0e342e90631c2dc71a4577c
Saeqa Sultani <saeqasultani@gmail.com>
2023-05-24 22:09:06 +0430

    fix linters

--- src/components/CarDetails/CarDetails.jsx
+++ src/components/CarDetails/CarDetails.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
-import styles from './style.module.css';
+import styles from './style.css';
 
 const CarDetails = () => {
   const { id } = useParams();
