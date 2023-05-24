a2aa6142997fe0dd50a4ab19298f4a4ba44e39f6
Saeqa Sultani <saeqasultani@gmail.com>
2023-05-24 18:37:01 +0430

    Edit the CarDetailspage.test.js

--- src/__test__/CarDetailspage.test.js
+++ src/__test__/CarDetailspage.test.js
@@ -1,11 +1,9 @@
-import { Provider } from 'react-redux';
-import store from '../Redux/configureStore';
-import CarDetails from '../Components/CarDetails/CarDetails';
 import renderer from 'react-test-renderer';
+import CarDetails from '../Components/CarDetails/CarDetails';
 
-it('Check the component', () => {
+it('Render the CarDetails component', () => {
   const tree = renderer.create(
-      <CarDetails />
+    <CarDetails />,
   ).toJSON();
   expect(tree).toMatchSnapshot();
 });
