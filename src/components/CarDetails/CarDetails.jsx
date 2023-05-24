99f9fbe02c3e72896333aaadd5805d51952c08b4
Saeqa Sultani <saeqasultani@gmail.com>
2023-05-24 22:42:39 +0430

    fix linters

--- src/components/CarDetails/CarDetails.jsx
+++ src/components/CarDetails/CarDetails.jsx
@@ -34,7 +34,6 @@ const CarDetails = () => {
             </p>
           </div>
           <button type="button" className={styles.reservebtn}>
-            <i className="fa-solid fa-car-side" />
                 Reserve
           </button>
         </div>
