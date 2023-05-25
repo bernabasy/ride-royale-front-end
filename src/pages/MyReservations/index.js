847bd078770135c68b317772af4c6e313ce51ef4
Saeqa Sultani <saeqasultani@gmail.com>
2023-05-25 18:59:24 +0430

    Fix linters

--- src/pages/MyReservations/index.js
+++ src/pages/MyReservations/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import Layout from '../../components/Layout/Layout';
-import MyReservations from '../../components/MyReservations/MyReservations';
 
 const MyReservationsPage = () => (
+
   <Layout>
 
-     <h1>My Reservations</h1>
+    <h1>My Reservations</h1>
 
   </Layout>
 );
