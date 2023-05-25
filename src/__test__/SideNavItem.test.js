62233387a5e593cf2f64c3230fea2da326f1f8d3
bernabasy <104406349+bernabasy@users.noreply.github.com>
2023-05-25 18:35:23 +0300

    Add tests for MatchSnapshot

---
+++ src/__test__/SideNavItem.test.js
@@ -0,0 +1,14 @@
+import { render } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import SideNav from '../components/SideNav';
+
+describe('SideNav tests', () => {
+    it('renders correctly', () => {
+      const tree = render(
+        <BrowserRouter>
+            <SideNav />
+          </BrowserRouter>
+      );
+      expect(tree).toMatchSnapshot();
+    });
+  });
